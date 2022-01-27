dcd88dabbef72cefe523eee539ef514720f94dbe
KaysPart <particulier.flore@gmail.com>
2022-01-27 14:32:20 +0100

    feat: fin page accueil

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const burgerMenu = document.getElementById('burger-menu');
 const nav = document.getElementById('main-nav');
+const modal = document.getElementById('modal');
+const openML = document.getElementById('open-ML')
+const closeML = document.getElementById('close-ML');
+
 
 burgerMenu.addEventListener('click', function(e){
     e.preventDefault();
     nav.classList.toggle('show');
     burgerMenu.classList.toggle('open');
 });
+
+openML.addEventListener('click', function(){
+    modal.classList.toggle('show')
+})
+
+closeML.addEventListener('click', function(){
+    modal.classList.toggle('show')
+})
